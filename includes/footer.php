cbeb88c541b57d738ceb0c4a5e5b1b918ef4c7fc
FabienDev <veyrat.fabien@gmail.com>
2015-05-26 10:56:02 +0200

    Revert "Base CMS"
    This reverts commit b44717ba8d05afe94a1fd81280e1ce6ed146755a.

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-		</div>
-		<footer>
-			<a href='mailto:nicolas.broisin@outlook.com'>Nicolas BROISIN DOUTAZ</a> - Ingénieur Communications Unifiées - Site réalisé par <a href='http://fabienveyrat.fr' target='blank'>Fabien VEYRAT</a>		
-		</footer>	
-	</body>
-</html>
