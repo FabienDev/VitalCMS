d5ceccff72fa379550200550a29bb9b1d11bf4c7
FabienDev <veyrat.fabien@gmail.com>
2015-05-28 10:12:51 +0200

    Update default.php

--- modules/mod_last_articles/views/default.php
+++ modules/mod_last_articles/views/default.php
@@ -1,6 +1,11 @@
 <h2>Nos derniers articles</h2>
 <ul>
+	<?php foreach($listing as $myArticle){ ?>
 	<li>
+		<a href="/articles/<?php echo $myArticle['url'];?>">
+			<?php echo $myArticle['titre'];
+			if(!empty($myArticle['date'])){
+				echo "Le ".$myArticle['date']; 
 			}?>
 		</a>
 	</li>
